946d09745b59b6d02d8cef85b5f6ae19e9ed0791
Cicero Leonardo da Silva <clsinfsolution@gmail.com>
2016-04-06 12:49:05 -0300

    Class newProva Class bimestre Diagramas Home

--- app/src/main/java/infsolution/com/br/infbullet/BulletinActivity.java
+++ app/src/main/java/infsolution/com/br/infbullet/BulletinActivity.java
@@ -4,15 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import infsolution.com.br.infbullet.dao.AlunoDAO;
+import infsolution.com.br.infbullet.modelo.Aluno;
 import infsolution.com.br.infbullet.modelo.Boletim;
 
 public class BulletinActivity extends AppCompatActivity {
-
+    private TextView nomeAluno;
+    private TextView media;
+    private Aluno aluno;
+    private AlunoDAO aluDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bulletin);
+        aluno=aluDao.getAluno();
+        aluDao.close();
+        if(aluno==null){
+            nomeAluno =(TextView)findViewById(R.id.txt_nome_aluno);
+            media = (TextView)findViewById(R.id.txt_media);
+        }else{
+            nomeAluno.setText(aluno.getNomeAluno().toString());
+        }
     }
     @Override
     protected void onResume() {
@@ -32,6 +46,10 @@ public class BulletinActivity extends AppCompatActivity {
         startActivity(goToCad);
     }
     public void goForProva(View v){
+        Intent goToCad = new Intent(this,NewProvaActivity.class);
+        startActivity(goToCad);
+    }
+    public void goForMateria(View v){
         Intent goToCad = new Intent(this,NewProvaActivity.class);
         startActivity(goToCad);
     }
